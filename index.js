e119a8116cbd071b7e75b961e07f78b74fe97dcf
sanjeev <sanjevsakya@gmail.com>
2018-10-21 11:05:28 +0545

    added ball for small accelerometer demo in mobile device

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 ;(function() {
   var app = document.getElementById('app');
 
-  var moInternal = createElementAndAppend('moInternal'); 
-  var moAccel = createElementAndAppend('moAccel');
-  var moAccelGrav = createElementAndAppend('moAccelGrav');
-  var moRotation = createElementAndAppend('moRotation');
-  var moInterval = createElementAndAppend('moInterval');
-  var moApi = createElementAndAppend('moApi');
+  var moInternal = createElementAndAppendTo('moInternal', app); 
+  var moAccel = createElementAndAppendTo('moAccel', app);
+  var moAccelGrav = createElementAndAppendTo('moAccelGrav', app);
+  var moRotation = createElementAndAppendTo('moRotation', app);
+  var moInterval = createElementAndAppendTo('moInterval', app);
+  var moApi = createElementAndAppendTo('moApi', app);
+
+  var garden = createElementAndAppendTo('garden', app);
+  var ball = createElementAndAppendTo('ball', garden);
+
+  var styles = {
+    gardenStyles: {
+      position: 'relative',
+      width : '200px',
+      height: '200px',
+      border: '5px solid #CCC',
+      borderRadius: '10px',
+    },
+    ball: {
+      position: 'absolute',
+      top: '90px',
+      left: '90px',
+      width: '20px',
+      height: '20px',
+      background: 'aqua',
+      borderRadius: '100%',
+    }
+  }
+  
+  applyStylesToElem(garden, styles.gardenStyles);
+  applyStylesToElem(ball, styles.ball);
+
+  function applyStylesToElem(elem, style = {}) {
+    Object.keys(style).forEach(function(st) {
+      elem.style[st] = style[st]
+    });
+  }
 
   app.appendChild(moInternal);
 
-  function createElementAndAppend(id) {
+  function createElementAndAppendTo(id, parent) {
       var elm = document.createElement('div');
       elm.setAttribute('id', id);
-      app.appendChild(elm);
+      parent.appendChild(elm);
       return elm;
   }
 
@@ -30,6 +61,7 @@
       lastReadingTimestamp = accelerometer.timestamp;
       accelerationHandler(accelerometer, moAccel);
     });
+
     accelerometer.start();
 
     if ('GravitySensor' in window) {
@@ -82,7 +114,35 @@
     info = xyz.replace('X', rotation.alpha && rotation.alpha.toFixed(3));
     info = info.replace('Y', rotation.beta && rotation.beta.toFixed(3));
     info = info.replace('Z', rotation.gamma && rotation.gamma.toFixed(3));
+
+    moveBall(info);
     moRotation.innerHTML = info;
+  }
+
+  var maxX = garden.clientWidth - ball.clientWidth;
+  var maxY = garden.clientHeight - ball.clientHeight;
+
+  function moveBall(rotation) {
+    var x = info[1];
+    var y = info[2];
+
+    if(typeof x === 'Number' && typeof y === 'Number') {
+      if(x > 90) {
+        x = 90;
+      };
+  
+      if(y < -90) {
+        y = -90
+      }
+  
+      x += 90;
+      y += 90;
+  
+      ball.style.top = (maxX * x /180 - 10) + 'px';
+      ball.style.left = (maxY * y /180 - 10) + 'px';
+    }
+
+    
   }
 
   function intervalHandler(interval) {
