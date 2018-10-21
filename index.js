813b9f7aaf7c58f8d97ea2af3e877062e66e2ce5
sanjeev <sanjevsakya@gmail.com>
2018-10-21 12:26:24 +0545

    simplyfy event listener

--- index.js
+++ index.js
@@ -48,78 +48,26 @@
     return elm;
   }
 
-  if ('LinearAccelerationSensor' in window && 'Gyroscope' in window) {
-    let lastReadingTimestamp;
-    let accelerometer = new LinearAccelerationSensor();
-
-    accelerometer.addEventListener('reading', e => {
-      if (lastReadingTimestamp) {
-        intervalHandler(
-          Math.round(accelerometer.timestamp - lastReadingTimestamp)
-        );
-      }
-      lastReadingTimestamp = accelerometer.timestamp;
-      accelerationHandler(accelerometer, moAccel);
-    });
-
-    accelerometer.start();
-
-    if ('GravitySensor' in window) {
-      let gravity = new GravitySensor();
-      gravity.addEventListener('reading', e =>
-        accelerationHandler(gravity, moAccelGrav)
-      );
-      gravity.start();
-    }
-
-    let gyroscope = new Gyroscope();
-    gyroscope.addEventListener('reading', e =>
-      rotationHandler({
-        alpha: gyroscope.x,
-        beta: gyroscope.y,
-        gamma: gyroscope.z,
-      })
-    );
-    gyroscope.start();
-  } else if ('DeviceMotionEvent' in window) {
-    moApi.innerHTML = 'Device Motion API';
-
-    var onDeviceMotion = function(eventData) {
-      accelerationHandler(eventData.acceleration, moAccel);
-      accelerationHandler(eventData.accelerationIncludingGravity, moAccelGrav);
-      rotationHandler(eventData.rotationRate);
-      intervalHandler(eventData.interval);
-    };
-
-    window.addEventListener('devicemotion', onDeviceMotion, false);
+  if('DeviceOrientationEvent' in window) {
+    window.addEventListener('deviceorientation', rotationHandler, true);
   } else {
     moApi.innerHTML = 'No Accelerometer & Gyroscope API available';
-  }
-
-  function accelerationHandler(acceleration, targetElem) {
-    var info,
-      xyz = '[X, Y, Z]';
-
-    info = xyz.replace('X', acceleration.x && acceleration.x.toFixed(3));
-    info = info.replace('Y', acceleration.y && acceleration.y.toFixed(3));
-    info = info.replace('Z', acceleration.z && acceleration.z.toFixed(3));
-    targetElem.innerHTML = info;
   }
 
   function rotationHandler(rotation) {
     var info,
       xyz = '[X, Y, Z]',
-      rotation;
+      rt;
 
     info = xyz.replace('Z', rotation.alpha && rotation.alpha.toFixed(3));
     info = info.replace('X', rotation.beta && rotation.beta.toFixed(3));
     info = info.replace('Y', rotation.gamma && rotation.gamma.toFixed(3));
 
-    rotation = {
+    rt = {
       X: (rotation.beta && rotation.beta.toFixed(3)) || 0,
       Y: (rotation.gamma && rotation.gamma.toFixed(3)) || 0,
     };
-    moveBall(rotation);
+    moveBall(rt);
     moRotation.innerHTML = info;
   }
 
